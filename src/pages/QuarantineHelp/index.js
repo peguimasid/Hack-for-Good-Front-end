9249f71afc6a812ff2bd8c7ef4dde7071d023955
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-04-05 18:17:52 -0400

    feature: Help list and Help create

--- src/pages/QuarantineHelp/index.js
+++ src/pages/QuarantineHelp/index.js
@@ -1,64 +1,157 @@
-import React from 'react';
-import { View, FlatList, Text, TouchableOpacity, Linking } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  Linking,
+  AsyncStorage,
+  ActivityIndicator
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { useNavigation } from "@react-navigation/native";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
 
-import styles from './styles';
+import api from "../../services/api";
+
+import styles from "./styles";
 
 export default function QuarantineHelp() {
+  const [loadingPage, setLoadingPage] = useState(false);
+  const [currentRegion, setCurrentRegion] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [helps, setHelps] = useState([]);
   const navigation = useNavigation();
 
-  const message = 'Olá, vi sua postagem no Conecte|Me e gostaria de ajudar você, como acha que seria a melhor forma para eu te auxiliar?'
+  const message =
+    "Olá, vi sua postagem no Conecte|Me e gostaria de ajudar você, como acha que seria a melhor forma para eu te auxiliar?";
+
+  async function loadHelp() {
+    try {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude
+        });
+        if (loadingPage) {
+          return;
+        }
+        if (total > 0 && helps.length === total) {
+          return;
+        }
+        setLoadingPage(true);
+        const { token } = JSON.parse(await AsyncStorage.getItem("Profile"));
+        const response = await api.get("help", {
+          headers: {
+            Authorization: `Barear ${token}`
+          },
+          params: {
+            latitude,
+            longitude,
+            page
+          }
+        });
+        setHelps([...helps, ...response.data]);
+        setTotal(response.headers["x-total-count"]);
+        setPage(page + 1);
+        setLoading(false);
+        setLoadingPage(false);
+      } else {
+        Alert.alert(
+          "Permissão necessária",
+          "O aplicativo requer permissão ao GPS"
+        );
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error.message);
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    loadHelp();
+  }, []);
+
+  async function refreshList() {
+    setLoading(true);
+    const { token } = JSON.parse(await AsyncStorage.getItem("Profile"));
+    const response = await api.get("help", {
+      headers: {
+        Authorization: `Barear ${token}`
+      },
+      params: currentRegion
+    });
+    setHelps(response.data);
+    setLoading(false);
+  }
 
   function navigateToMainPage() {
-    navigation.navigate("Main")
+    navigation.navigate("Main");
   }
 
   function navigateToPostToHelpPage() {
-    navigation.navigate("PostToHelp")
+    navigation.navigate("PostToHelp");
   }
 
-  function sendWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=+5524988395623&text=${message}`);
+  function sendWhatsapp(help) {
+    Linking.openURL(`whatsapp://send?phone=+55${help.phone}&text=${message}`);
   }
 
   return (
     <>
-    <View style={styles.header}>
-    <TouchableOpacity onPress={navigateToMainPage}>
-      <View style={styles.back}>
-        <Icon name="chevron-left" size={32} color="#333" />
-        <Text style={styles.backTitle}>Voltar</Text>
-      </View>
-    </TouchableOpacity>
-
-      <View style={styles.addHelp}>
-        <TouchableOpacity onPress={navigateToPostToHelpPage}>
-          <Icon name="plus" size={32} color='#333' />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={navigateToMainPage}>
+          <View style={styles.back}>
+            <Icon name="chevron-left" size={32} color="#333" />
+            <Text style={styles.backTitle}>Voltar</Text>
+          </View>
         </TouchableOpacity>
-      </View>
-    </View>
 
-    <FlatList 
-      data={[1, 2]}
-      style={styles.postsContainer}
-      keyExtractor={help => String(help)}
-      renderItem={() => (
-        <View style={styles.helpList}>
-        <View style={styles.help}>
-          <Text style={styles.helpTitle}>Alguem para ir à farmácia para mim.</Text>
-          <Text style={styles.helpDescription}>Preciso que alguem vá à farmacia para mim poir preciso de um remédio para minha doenca, já estou sem à 3 dias.</Text>
-        </View>
-
-        <View style={styles.contactHelp}>
-          <TouchableOpacity onPress={sendWhatsapp}>
-            <Icon name="comments" size={32} color='#4f4f4f' />
+        <View style={styles.addHelp}>
+          <TouchableOpacity onPress={navigateToPostToHelpPage}>
+            <Icon name="plus" size={32} color="#333" />
           </TouchableOpacity>
-          <Text style={styles.distance}> -5 \Km</Text>
         </View>
       </View>
-      )}
-    />
+      <FlatList
+        data={helps}
+        refreshing={loading}
+        onRefresh={refreshList}
+        onEndReached={loadHelp}
+        onEndReachedThreshold={0.2}
+        style={styles.postsContainer}
+        keyExtractor={help => String(help.id)}
+        renderItem={({ item: help }) => (
+          <View style={styles.helpList}>
+            <View style={styles.help}>
+              <View>
+                <Text style={styles.helpTitle}>{help.title}</Text>
+                <Text style={styles.helpDescription}>{help.description}</Text>
+              </View>
+            </View>
+
+            <View style={styles.contactHelp}>
+              <TouchableOpacity onPress={() => sendWhatsapp(help)}>
+                <Icon name="comments" size={32} color="#4f4f4f" />
+              </TouchableOpacity>
+              <Text style={styles.distance}> +/- {help.distance} \Km</Text>
+            </View>
+          </View>
+        )}
+      />
     </>
   );
 }
--- src/pages/QuarantineHelp/styles.js
+++ src/pages/QuarantineHelp/styles.js
@@ -1,67 +1,72 @@
-import { StyleSheet } from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { StyleSheet } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 export default StyleSheet.create({
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: wp('6'),
-    paddingTop: hp('7'),
-    paddingBottom: hp('2')
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: wp("6"),
+    paddingTop: hp("7"),
+    paddingBottom: hp("2")
   },
 
   back: {
-    flexDirection: 'row',
+    flexDirection: "row"
   },
 
   helpList: {
-    paddingBottom: hp('3')
+    paddingBottom: hp("3")
   },
 
   backTitle: {
-    color: '#333',
-    marginTop: hp('1'),
-    marginLeft: wp('2'),
+    color: "#333",
+    marginTop: hp("1"),
+    marginLeft: wp("2")
   },
 
   postsContainer: {
-    paddingHorizontal: wp('5'),
+    paddingHorizontal: wp("5")
   },
-  
+
   help: {
-    backgroundColor: '#rgba(10, 180, 255, 0.5)',
-    paddingHorizontal: wp('5'), 
-    paddingVertical: hp('3'),
+    backgroundColor: "#rgba(10, 180, 255, 0.5)",
+    paddingHorizontal: wp("5"),
+    paddingVertical: hp("3"),
     borderTopStartRadius: 5,
-    borderTopEndRadius: 5
+    borderTopEndRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
 
   helpTitle: {
-    fontSize: hp('2.5'),
-    color: '#fff',
-    fontWeight: '600',
-    marginBottom: hp('1')
+    fontSize: hp("2.5"),
+    color: "#fff",
+    fontWeight: "600",
+    marginBottom: hp("1")
   },
 
   helpDescription: {
-    fontSize: hp('1.8'),
-    color: '#fff',
-    fontWeight: '200',
+    fontSize: hp("1.8"),
+    color: "#fff",
+    fontWeight: "200"
   },
 
   contactHelp: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#fff',
-    paddingHorizontal: wp('5'),
-    paddingVertical: hp('0.6'),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    paddingHorizontal: wp("5"),
+    paddingVertical: hp("0.6"),
     borderBottomStartRadius: 5,
     borderBottomEndRadius: 5
   },
 
   distance: {
-    color: '#4f4f4f',
-    fontWeight:'300',
-    marginTop: hp('1')
+    color: "#4f4f4f",
+    fontWeight: "300",
+    marginTop: hp("1")
   }
-})
+});
